4f70b6255da84ead0309251dd56f3c1efa47899a
mtdtao <mtdtao@gmail.com>
2016-04-27 21:20:24 -0400

    test auth

--- index.js
+++ index.js
@@ -35,6 +35,27 @@ var api = new ParseServer({
 
 var app = express();
 
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var Pusher = require('pusher');
+
+// var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var pusher = new Pusher({ appId: APP_ID, key: APP_KEY, secret:  APP_SECRET });
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
