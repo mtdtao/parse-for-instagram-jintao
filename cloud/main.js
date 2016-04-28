08d792e3aeb5efbc26b5c6ae74b7fcab5a57f375
mtdtao <mtdtao@gmail.com>
2016-04-27 23:03:05 -0400

    test

--- cloud/main.js
+++ cloud/main.js
@@ -24,3 +24,11 @@ Parse.Cloud.define('pushertest', function(req, res) {
   res.success('pusher hi');
 });
 
+Parse.Cloud.define('authorizePusherChannel', function (request, response) {
+	if (!request.user) { response.error('User should be autenticated.'); }
+	var pusherAppKey = 'your-pusher-app-key';
+	var pusherAppSecret = 'your-pusher-app-secret';
+	var stringToSign = request.params.socketid + ':' + request.params.channelname;
+	var authToken = pusherAppKey + ':' + crypto.createHmac('sha256', pusherAppSecret).update(stringToSign).digest('hex');
+	response.success(JSON.stringify({auth:authToken}));
+});
